dbd1c80c85846547e8018fb105d4cf834a1a2d42
Amirabbas Fazelinia <utilyre@proton.me>
2023-10-16 13:31:33 +0330

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.post.getAll.useQuery();
 
   return (
     <>
@@ -16,8 +16,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {user.isSignedIn && <SignOutButton />}
-        {!user.isSignedIn && <SignInButton />}
+        <div>
+          {user.isSignedIn && <SignOutButton />}
+          {!user.isSignedIn && <SignInButton />}
+        </div>
+
+        <div>
+          {data?.map((post) => <div key={post.id}>{post.content}</div>)}
+        </div>
       </main>
     </>
   );
